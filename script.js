752de4af07b4378c8997bd04b5bbc4e298baf7a3
verilytech <verilyclaude@proton.me>
2026-03-17 23:38:02 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+try {
 const utterance = new SpeechSynthesisUtterance("hello");
 speechSynthesis.speak(utterance);
 console.log("hi?");
+} catch (err) {
+ document.getElementById("test").innerText = err.toString();
+}
